7f26f0e5d077abc065bcc6a337f6069a1b076fd0
zip6como <79743601+zip6como@users.noreply.github.com>
2023-07-24 13:05:58 +0200

    Add files via upload
    Sends the house.txt file to the discord API so that you get the output to your discord server.

---
+++ webhook.py
@@ -0,0 +1,33 @@
+﻿#THIS FILE SENDS THE HOUSE.TXT FILE CONTENT TO THE WEBHOOK
+import requests
+
+def send_to_discord_webhook(webhook_url, file_path):
+    try:
+        # Read the content of the file
+        with open(file_path, 'r') as file:
+            content = file.read()
+
+        # Prepare the payload to be sent to the Discord webhook
+        payload = {
+            'content': content
+        }
+
+        # Send the payload to the webhook URL
+        response = requests.post(webhook_url, json=payload)
+
+        # Check if the request was successful
+        if response.status_code == 204:
+            pass
+        else:
+            print("Failed to send content to Discord. Status code:", response.status_code)
+
+    except FileNotFoundError:
+        print("File not found. Please check the file path.")
+
+#THIS IS TO CONFIGURE THE WEBHOOK
+if __name__ == "__main__":
+    webhook_url = "INPUT YOUR WEBHOOK HERE"
+    file_path = "house.txt"  # Update the file path accordingly
+#-CONFIG END-
+
+    send_to_discord_webhook(webhook_url, file_path)
